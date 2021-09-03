f2041904cb8bf4858071032ce968c4b85602b7be
vzhabchyk <rvromanovna@gmail.com>
2021-09-03 13:05:30 +0300

    changes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const fullName = prompt("Введите ваше имя и фамилию!");
+const firstName = prompt("Введите ваше имя.");
+const lastName = prompt("Введите вашу фамилию.")
 const age = confirm("Вам есть 18 лет?");
 
 if (age) {
-  alert("Добро пожаловать " + fullName + " вот ваши сигареты");
+  alert("Добро пожаловать, " + firstName + " " + lastName + ", вот ваши сигареты");
 } else {
-  alert("Извините, " + fullName + " Вам нет 18 лет!");
+  alert("Извините, " + firstName + " " + lastName + ", Вам нет 18 лет!");
 }
